4b73f8e566d292b92545940ea2253d10386e6b95
Aleksander Blaszkiewicz <kqmdjc8@gmail.com>
2023-05-24 17:57:55 +0200

    Rename 1

--- sri/node-soap-example-master/app.js
+++ sri/node-soap-example-master/app.js
@@ -1,6 +1,5 @@
 /*jslint node: true */
-"use strict";
-
+'use strict';
 
 var soap = require('soap');
 var express = require('express');
@@ -8,28 +7,25 @@ var fs = require('fs');
 
 // the splitter function, used by the service
 function splitter_function(args) {
-    console.log('splitter_function');
-    var splitter = args.splitter;
-    var splitted_msg = args.message.split(splitter);
-    var result = [];
-    for(var i=0; i<splitted_msg.length; i++){
-      result.push(splitted_msg[i]);
-    }
-    return {
-        result: result
-        }
+  console.log('splitter_function');
+  var splitter = args.splitter;
+  var splitted_msg = args.message.split(splitter);
+  var result = [];
+  for (var i = 0; i < splitted_msg.length; i++) {
+    result.push(splitted_msg[i]);
+  }
+  return {
+    result: result,
+  };
 }
 
 // the service
 var serviceObject = {
-  MessageSplitterService: {
-        MessageSplitterServiceSoapPort: {
-            MessageSplitter: splitter_function
-        },
-        MessageSplitterServiceSoap12Port: {
-            MessageSplitter: splitter_function
-        }
-    }
+  AppService: {
+    AppServiceSoapPort: {
+      MessageSplitter: splitter_function,
+    },
+  },
 };
 
 // load the WSDL file
@@ -40,13 +36,13 @@ var app = express();
 // root handler
 app.get('/', function (req, res) {
   res.send('Node Soap Example!<br /><a href="https://github.com/macogala/node-soap-example#readme">Git README</a>');
-})
+});
 
 // Launch the server and listen
 var port = 8000;
 app.listen(port, function () {
   console.log('Listening on port ' + port);
-  var wsdl_path = "/wsdl";
+  var wsdl_path = '/wsdl';
   soap.listen(app, wsdl_path, serviceObject, xml);
-  console.log("Check http://localhost:" + port + wsdl_path +"?wsdl to see if the service is working");
+  console.log('Check http://localhost:' + port + wsdl_path + '?wsdl to see if the service is working');
 });
